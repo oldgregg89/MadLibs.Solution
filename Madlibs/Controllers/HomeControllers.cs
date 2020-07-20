f4c35aba867b6262f28738ae3ca6a3af14a7ea49
Ian Scott <chesnekov@gmail.com>
2020-07-20 14:43:43 -0700

    updates project with home page with buttons that link to each several forms for different madlibs
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- Madlibs/Controllers/HomeControllers.cs
+++ Madlibs/Controllers/HomeControllers.cs
@@ -14,6 +14,9 @@ namespace Madlibs.Controllers
     [Route("/form2")]
     public ActionResult Form2() { return View(); }
 
+    [Route("/")]
+    public ActionResult ChooseAMadlib () { return View(); }
+
     [Route("/MadlibPark")]
     public ActionResult MadlibPark (string adjective1, string adjective2, string noun1, string noun2, string noun3, string verb1, string verb2, string clothing, string feeling) 
     {
@@ -30,28 +33,22 @@ namespace Madlibs.Controllers
       return View(myMadlib);
     }
 
-    [Route("/")]
-    public ActionResult ChooseAMadlib ()
-    {
-      
-    }
-
     [Route("/MadlibAlbert")]
     public ActionResult MadlibAlbert (string place, string adjective1, string adjective2, string femaleCelebrity, string maleCelebrity, string noun1, string noun2, string noun3, string pluralNoun1, string pluralNoun2, string pluralNoun3, string pluralNoun4, string pluralProfession) 
     {
-      AlbertEinstein myMadlib = new AlbertEinstein();
+      EinsteinStory myMadlib = new EinsteinStory();
       myMadlib.FemaleCelebrity = femaleCelebrity;
       myMadlib.MaleCelebrity = maleCelebrity;
-      myMadlib.Noun1 = noun1;
-      myMadlib.PluralNoun1 = pluralNoun1;
       myMadlib.Adjective1 = adjective1;
-      myMadlib.PluralNoun2 = pluralNoun2;
       myMadlib.Adjective2 = adjective2;
-      myMadlib.PluralNoun3 = pluralNoun3;
+      myMadlib.Noun1 = noun1;
       myMadlib.Noun2 = noun2;
-      myMadlib.Place = place;
-      myMadlib.PluralNoun4 = pluralNoun4;
       myMadlib.Noun3 = noun3;
+      myMadlib.PluralNoun1 = pluralNoun1;
+      myMadlib.PluralNoun2 = pluralNoun2;
+      myMadlib.PluralNoun3 = pluralNoun3;
+      myMadlib.PluralNoun4 = pluralNoun4;
+      myMadlib.Place = place;
       myMadlib.PluralProfession = pluralProfession;
       return View(myMadlib);
     }
